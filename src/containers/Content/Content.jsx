9e103036a67f702701a706b7d1b967f94b9aa09c
Liza <83118013+li3k4@users.noreply.github.com>
2022-09-05 13:25:17 +0200

    feat(new)

--- src/containers/Content/Content.jsx
+++ src/containers/Content/Content.jsx
@@ -3,20 +3,14 @@ import styles from "./Content.module.css";
 import PrivacyPolicy from "../PrivacyPolicy";
 
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-
-import icon from "../../resources/logo_black.svg";
-import left from "../../resources/left.png";
-import right from "../../resources/right.png";
-
+import img_main from "../../resources/img.png";
+import main from "../../resources/img.png";
 const Content = () => {
   return (
     <div className={styles["container"]}>
-      <div className={styles["content"]}>
-        <div className={styles["txt-icon"]}>
-          <div className={styles["icon"]}>
-            <img src={icon} />
-          </div>
-          <div className={styles["item text"]}>
+         <img className={styles["item-1"]} src={main}></img>
+         <div className={styles["item-2"]}>
+         <div className={styles["item-text"]}>
             <h1>
               “Пятое измерение” - практики <br />
               способные изменить твою <br />
@@ -28,14 +22,15 @@ const Content = () => {
               внутреннего потенциала.
             </h2>
           </div>
-          <button className={styles["btn"]}> Скачать приложение для Android</button>
-        </div>
-
-        <NavLink to="/policy" element={PrivacyPolicy} className={styles["pp"]}>
+          <button className={styles["btn"]}>  Скачать на Android
+          </button>
+          <NavLink to="/policy" element={PrivacyPolicy} className={styles["pp"]}>
           {" "}
           Политика конфиденциальности{" "}
         </NavLink>
-      </div>
+         </div>
+          
+
     </div>
   );
 };
