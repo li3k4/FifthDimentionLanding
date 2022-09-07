57500e54c3f4b6b9e5d8d9798b43d5f500263f0f
Liza <83118013+li3k4@users.noreply.github.com>
2022-09-07 15:34:27 +0200

    feat(version 2.1.5)

--- src/containers/Content/Content.jsx
+++ src/containers/Content/Content.jsx
@@ -3,34 +3,33 @@ import styles from "./Content.module.css";
 import PrivacyPolicy from "../PrivacyPolicy";
 
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-import img_main from "../../resources/img.png";
 import main from "../../resources/img.png";
 const Content = () => {
   return (
     <div className={styles["container"]}>
-         <img className={styles["item-1"]} src={main}></img>
-         <div className={styles["item-2"]}>
-         <div className={styles["item-text"]}>
-            <h1>
-              “Пятое измерение” - практики <br />
-              способные изменить твою <br />
-              жизнь к лучшему
-            </h1>
-            <h2>
-              Саморазвивайся в обасти семьи, бизнеса, <br />
-              духовного здоровья, эмойций и <br />
-              внутреннего потенциала.
-            </h2>
-          </div>
-          <button className={styles["btn"]}>  Скачать на Android
-          </button>
-          <NavLink to="/policy" element={PrivacyPolicy} className={styles["pp"]}>
+      <div className={styles["item-1"]}>
+      <img className={styles["item-1-img"]} src={main}/>
+      </div>
+
+
+      <div className={styles["item-2"]}>
+        <div className={styles["item-text"]}>
+          <h1>
+            “Пятое измерение” - практики, <br />
+            способные изменить твою <br />
+            жизнь к лучшему
+          </h1>
+          <h2>
+          Саморазвивайся в области здоровья, любви, отношений, денег, концентрации, предназначения.
+         Здесь ты найдешь свои личные ключи к Счастью.
+          </h2>
+        </div>
+        <button className={styles["btn"]}> Скачать на Android</button>
+        <NavLink to="/policy" element={PrivacyPolicy} className={styles["pp"]}>
           {" "}
           Политика конфиденциальности{" "}
         </NavLink>
-         </div>
-          
-
+      </div>
     </div>
   );
 };
